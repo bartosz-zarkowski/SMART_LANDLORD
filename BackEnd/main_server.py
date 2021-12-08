b2fecb6b2a67278cc6cb586cb16d2410a8d93bb7
Xaness <officialkacper@gmail.com>
2021-12-08 16:39:51 +0000

    Rozwiniecie funkcji powiadomien

--- BackEnd/main_server.py
+++ BackEnd/main_server.py
@@ -1,15 +1,24 @@
 import socket
+import socketserver
 from notification import *
+
 s=socket.socket()
+print('socket created')
 port =3389
 s.bind(('',port))
+print('socket binded to %s' %(port))
 s.listen(5)
-email_password=input("Podaj haslo do maila: ")
+print('socket is listening')
+email_password=input('Podaj haslo do maila:')
 
 while True:
-    c,addr=s.accept()
+    c,addr = s.accept()
+    print('got connection form',addr)
+    c.send('Ty for connecting'.encode())
     if c:
-        sendEmail("officialkacper@gmail.com",email_password)
-        c.send()
-        c.close()
+        print('wysylam maila')
+        sendEmail("kk48302@zut.edu.pl",email_password)
+    c.close()
     break
+
+
